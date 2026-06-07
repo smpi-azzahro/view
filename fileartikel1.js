d6fd599eea21c86238155434aaa9093e21531d8e
smpi-azzahro <smpi.azzahro@gmail.com>
2026-06-07 18:23:57 +0700

    Delete fileartikel1.js

--- fileartikel1.js
+++
@@ -1,6 +0,0 @@
-artikelData = {
-  "judul": "Pelaksanaan Pondok Ramadhan 1447 H di SMP Islam Az-Zahro",
-  "gambar": "https://images.unsplash.com/photo-1541339907198-e08756dedf3f",
-  "tanggal": "April 15, 2026",
-  "konten": "<p>Hari pertama diisi dengan pembukaan oleh Kepala Sekolah serta pemetaan kelompok hafalan juz harian.</p><p>Diharapkan siswa mampu istiqomah menjalankan ibadah sunnah sepanjang bulan suci ini.</p>"
-};
